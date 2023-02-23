c432c14c8883941e6c77d6092657cd8835a94e86
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2023-02-23 14:20:47 +0200

    Add paragraph to elements index

--- src/Elements/index.tsx
+++ src/Elements/index.tsx
@@ -2,10 +2,12 @@ import { Button } from './Button/Button';
 import { Heading } from './Heading/Heading';
 import { Input } from './Input/Input';
 import { Label } from './Label/Label';
+import { Paragraph } from './Paragraph/Paragraph';
 
 export {
   Button,
   Heading,
   Input,
   Label,
+  Paragraph,
 };
