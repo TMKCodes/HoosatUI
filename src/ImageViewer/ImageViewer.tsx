b7fac946131e50f888aee25524238bd0354abc74
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-04-05 12:34:09 +0300

    Add ImageViewer to show markdown images in fullscreen display that is closable.

---
+++ src/ImageViewer/ImageViewer.tsx
@@ -0,0 +1,17 @@
+import React, { ImgHTMLAttributes } from "react";
+import { Button, Image } from "../Elements";
+
+import "./ImageViewer.scss";
+
+interface ImageViewerProps extends ImgHTMLAttributes<HTMLImageElement> {
+  onClose: () => void;
+}
+
+export const ImageViewer: React.FC<ImageViewerProps> = (props) => {
+  return (
+    <div className="imageViewer">
+      <Button className="closeButton" onClick={() => { props.onClose(); }}>X</Button>
+      <Image alt={props.alt} src={props.src}></Image>
+    </div>
+  )
+}
--- src/Markdown/Markdown.tsx
+++ src/Markdown/Markdown.tsx
@@ -1,42 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
 import rehypeHighlight from 'rehype-highlight'
 
 import "./Markdown.scss";
+import { ImageViewer } from '../ImageViewer/ImageViewer';
 
 interface MarkdownProps {
   markdown: string;
   className?: string;
 }
 
+interface showImageDTO {
+  src: string | undefined,
+  alt: string | undefined,
+}
+
 export const Markdown: React.FC<MarkdownProps> = (rest) => {
+  const [showImage, setShowImage] = useState<showImageDTO>({ src: undefined, alt: undefined});
   return (
-    <ReactMarkdown 
-      className={`gfm ${rest.className}`}
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeHighlight]}
-      components={{
-        code: ({node, inline, className, children, ...props}) => {
-          const language = className && className.replace(/language-/, '');
-          return (
-            <code className={`language-${language}`} {...props}>
-              {children}
-            </code>
-          );
-        },
-        img: ({ node, className, children, ...props }) => {
-          return (
-            <img {...props} src={props.src} alt={props.alt} className={className} onClick={(e) => {
-              console.log(e);
-            }}>
-              {children}
-            </img>
-          );
-        },
-      }}
-      >
-      { rest.markdown }
-    </ReactMarkdown>
+    <>
+      { (showImage.src !== undefined) && 
+        <ImageViewer 
+          src={showImage.src} 
+          alt={showImage.alt}
+          onClose={() => { setShowImage({ ...showImage, src: undefined, alt: undefined })}}
+        >
+        </ImageViewer>
+      }
+      <ReactMarkdown 
+        className={`gfm ${rest.className}`}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeHighlight]}
+        components={{
+          code: ({node, inline, className, children, ...props}) => {
+            const language = className && className.replace(/language-/, '');
+            return (
+              <code className={`language-${language}`} {...props}>
+                {children}
+              </code>
+            );
+          },
+          img: ({ node, className, children, ...props }) => {
+            return (
+              <img {...props} src={props.src} alt={props.alt} className={className} onClick={() => {
+                  setShowImage({ ...showImage, src: props.src, alt: props.alt });
+                }}>
+                {children}
+              </img>
+            );
+          },
+        }}
+        >
+        { rest.markdown }
+      </ReactMarkdown>
+    </>
   )
 }
