77dc19fa968a6e11dca262d9292fd7d81495a0a8
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-07-06 18:13:21 +0300

    Add image width and height control to markdown.

--- src/Markdown/Markdown.tsx
+++ src/Markdown/Markdown.tsx
@@ -42,13 +42,33 @@ export const Markdown: React.FC<MarkdownProps> = (rest) => {
             );
           },
           img: ({ node, className, children, ...props }) => {
-            return (
-              <img {...props} src={props.src} alt={props.alt} className={className} onClick={() => {
-                  setShowImage({ ...showImage, src: props.src, alt: props.alt });
-                }}>
-                {children}
-              </img>
-            );
+            console.log(props.src);
+            const srcSplit = props.src?.split("%7Cdimensions=");
+            console.log("img srcSplit:" + srcSplit?.length);
+            if (srcSplit && srcSplit.length > 1) {
+              console.log("srcSplit[0]: " + srcSplit[0]);
+              props.src = srcSplit[0]; 
+              console.log(srcSplit[1]);
+              const width = srcSplit[1].split("x")[0];
+              const height = srcSplit[1].split("x")[1];
+              console.log(width);
+              console.log(height);
+              return (  
+                <img {...props} src={props.src} alt={props.alt} className={className} style={{ maxWidth: width + "px", maxHeight: height + "px" }} onClick={() => {
+                    setShowImage({ ...showImage, src: props.src, alt: props.alt });
+                  }}>
+                  {children}
+                </img>
+              );
+            } else {
+              return (  
+                <img {...props} src={props.src} alt={props.alt} className={className} onClick={() => {
+                    setShowImage({ ...showImage, src: props.src, alt: props.alt });
+                  }}>
+                  {children}
+                </img>
+              );
+            }
           },
         }}
         >
